92e5f4ae7b3bb8f500e0c55f27717841d7516241
msualp <msualp@sociail.com>
2025-04-10 22:08:54 -0400

    Fix header issues by replacing ConsolidatedHeader with MacHeader

--- components/ThemeWrapper.tsx
+++ components/ThemeWrapper.tsx
@@ -2,7 +2,7 @@
 
 import { ReactNode, useEffect, useState } from 'react'
 import { ThemeProvider } from 'next-themes'
-import { ConsolidatedHeader } from './ConsolidatedHeader'
+import { MacHeader } from './MacHeader'
 import { Footer } from './Footer'
 import { CallToAction } from './CallToAction'
 
@@ -24,8 +24,8 @@ export function ThemeWrapper({
 
   return (
     <ThemeProvider attribute="class" enableSystem={true}>
-      {/* Consolidated Header */}
-      <ConsolidatedHeader />
+      {/* Header */}
+      <MacHeader />
 
       {/* Main content offset to account for fixed header */}
       <main className="pt-16">{children}</main>
